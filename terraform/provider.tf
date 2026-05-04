141997a16a34edb0d9e8a21ab8dad204d88b0b54
Nitanshu12 <nitanshugoyal786@gmail.com>
2026-05-04 13:00:53 +0530

    Configure S3 remote backend for Terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.3.0"
 
+  backend "s3" {
+    bucket         = "collabsphere-tf-state-953907694232"
+    key            = "terraform/state.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "collabsphere-tf-lock"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
